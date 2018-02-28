bd6383642c59cee01ed2393df631aa59e3776887
Thomas Lemqvist <dr.bumlehund@live.dk>
2018-02-28 12:05:31 +0100

    figured out we should make a new plot script - should have read the instructions... sorry

--- Lab_4/visualization/script2.1.3.py
+++ Lab_4/visualization/script2.1.3.py
@@ -37,29 +37,38 @@ def plot(path, inches):
 
     # Set x and y values
     x = df['Power']
-    y1 = df['Thrust CW AVG']
-    y2 = df['Thrust CCW AVG']
+    y1 = df['Thrust CW']
+    y2 = 0
+    if inches is not 8:
+        y2 = df['Thrust CCW']
 
     # define labels for the legend
-    cw = pch.Patch(color='orange', label='CW')
-    ccw = pch.Patch(color='teal', label='CCW')
-    reg_cw = pch.Patch(color='red', label='CW trend')
-    reg_ccw = pch.Patch(color='blue', label='CCW trend')
-    plt.legend(handles=[cw, ccw, reg_cw, reg_ccw])
+    legends = [pch.Patch(color='orange', label=str(inches) + '\" CW'),
+               pch.Patch(color='red', label=str(inches) + '\" CW trend')]
+    if inches is not 8:
+        legends.append(pch.Patch(color='teal', label=str(inches) + '\" CCW'))
+        legends.append(pch.Patch(color='blue', label=str(inches) + '\" CCW trend'))
+    plt.legend(handles=legends)
 
     # add lines to the plot
     plt.scatter(x, y1, color='orange')
-    plt.scatter(x, y2, color='teal')
+    if inches is not 8:
+        plt.scatter(x, y2, color='teal')
 
     # regression for CW
     p = np.polyfit(x, y1, 3)
     f_cw = np.poly1d(p)
     plt.plot(x, f_cw(x), color='red')
 
+    trends[str(inches) + '\" CW'] = f_cw
+
     # regression for CCW
-    p = np.polyfit(x, y2, 3)  # calculate third degree polynomial trend line
-    f_ccw = np.poly1d(p)
-    plt.plot(x, f_ccw(x), color='blue')
+    if inches is not 8:
+        p = np.polyfit(x, y2, 3)  # calculate third degree polynomial trend line
+        f_ccw = np.poly1d(p)
+        plt.plot(x, f_ccw(x), color='blue')
+
+        trends[str(inches) + '\" CCW'] = f_ccw
 
     # set plot axis labels
     plt.xlabel('Power')
@@ -70,11 +79,9 @@ def plot(path, inches):
     # show plot
     plt.show()
 
-    trends[str(inches) + '\" CW'] = f_cw
-    trends[str(inches) + '\" CCW'] = f_ccw
-
 
 plot('Thrust curve - 8 inch.csv', 8)
+plot('Thrust curve - 10 inch.csv', 10)
 
 # define possible colors for trend plot
 colors = ['red', 'blue', 'green', 'yellow']
@@ -82,7 +89,8 @@ colors = ['red', 'blue', 'green', 'yellow']
 legend = []
 # loop through the trends
 for name, f in trends.items():
-    print('name=' + str(name) + ' f=' + str(f))
+    print('name=' + str(name))
+    print('f=' + str(f))
     col = colors.pop()  # get color
     legend.append(pch.Patch(color=col, label=name))  # create legend entry
     plt.plot(x, f(x), color=col)  # add line to plot
