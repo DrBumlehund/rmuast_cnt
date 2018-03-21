e4c1c1fa43b02320728dc24ba3b41d2f99a8a2c6
Thomas Lemqvist <dr.bumlehund@live.dk>
2018-03-21 11:20:57 +0100

    added project for lab_7

---
+++ Lab_7/code/script.py
@@ -0,0 +1,43 @@
+#
+#  Copyright 2018 Chris Bang Sørensen, Niels Hvid, Thomas Lemqvist
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#  of the Software, and to permit persons to whom the Software is furnished to do so,
+#  subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+#  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+#  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+# Packages to install: (pip install)
+#       pandas, matplotlib, xlrd, numpy
+#
+
+
+import pandas as pd  # for storing data
+import numpy as np  # for math
+from datetime import datetime as time
+import datetime
+import matplotlib.pyplot as plt  # for plotting
+import matplotlib.dates as dt
+import matplotlib.lines as ln
+from math import pi, cos, sqrt, sin, asin
+
+
+class Data:
+    def __init__(self):
+        print('hej')
+
+
+if __name__ == '__main__':
+    d = Data()
